9ba0ce4aa081a7fc86d96bfa4b2b796f08d72f7a
PietroMenottii <menottipietro@gmail.com>
2025-11-20 18:56:18 +0100

    Add files via upload

---
+++ Code/position.py
@@ -0,0 +1,32 @@
+MANDATORY_POSITION = {
+    'StandInit': 2.000000,
+    'Hello': 6.284200,
+    'Stand': 2.029481,
+    'Sit': 17.574000,
+    'Wipe_Forehead': 6.978000,
+    'StandZero': 1.931000,
+    'SitRelax': 19.842000,
+    'Crouch': 2.264000
+}
+
+POSITION = {
+    'DanceMove': 8.120267,
+    'Mani_sui_fianchi':  2.408533,
+    'Arms_opening': 9.958400,
+    'Dab': 4.063400,
+    'Move_forward': 3.827533,
+    'PulpFiction': 7.546,
+    'ComeOn': 4.740000,
+    'Diagonal_left': 3.246467,
+    'Stand_from_sit': 10.677000,
+    'Move_backward': 4.087800,
+    'Ballo_braccia': 7.377400,
+    'Right_arm': 13.298000,
+    'Right_sprinkler': 11.939000,
+    'Rotation_foot_LLeg': 10.838267,
+    'Union_arms': 11.377333,
+    'Diagonal_right': 3.289267,
+    'AirGuitar': 5.425467,
+    'TheRobot': 8.287933,
+    'Happy_Birthday': 13.300667,
+}
